8d26d55308075a03e6ea211ff8bb2fb415cb5fa4
Fjorge <30599715+FjorgeTheHuman@users.noreply.github.com>
2021-03-04 18:06:47 +0000

    add missed changes from previous PR (#15)
    * add remove support, add multipkg support
    
    * remake argument parsing; fix errors
    
    * fix error
    
    * fix pkgs not being given to function
    
    * remake text for multi-package support
    
    * fix using wrong var
    
    * fix stuff
    
    * fix different but same stuff
    
    * change add to remove in remove.py
    
    * fix keyerror
    
    * Better error "handling"
    
    * move operations list
    
    Co-authored-by: Fjorge <FjorgeTheHuman@github.com>

--- libtwirl/__main__.py
+++ libtwirl/__main__.py
@@ -3,9 +3,6 @@ from libtwirl.core.install import pkg_install
 from libtwirl.core.remove import pkg_remove
 from sys import argv
 
-args = argv[1:]
-pkgs = args[1:]
-
 # List of actions
 operations = {
 	"install": pkg_install,
@@ -14,12 +11,19 @@ operations = {
 	"search": None
 }
 
+# Check for args
+if not len(argv) > 1:
+	raise IncorrectArgumentFormatError(f"No operation speficied. Valid operations are {', '.join(operations.keys())}.")
+
+args = argv[1:]
+
 # Check for argument errors
-if not operations[args[0]]:
+if not args[0] in operations.keys():
 	raise IncorrectArgumentFormatError(f"Unknown operation '{args[0]}'. Valid operations are {', '.join(operations.keys())}.")
 if not len(args) > 1:
 	raise IncorrectArgumentFormatError("No packages specified.")
 
+# Set pkgs after checks to avoid index out of range errors
+pkgs = args[1:]
 # Do operations
 operations[args[0]](pkgs)
-

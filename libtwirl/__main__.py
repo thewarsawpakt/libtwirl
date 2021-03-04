65676735546000e35a73b1f503163e607fca4783
Fjorge <30599715+FjorgeTheHuman@users.noreply.github.com>
2021-03-04 17:47:54 +0000

    add uninstalling functionality; redo arg parser; multi-package support (#13)
    * add remove support, add multipkg support
    
    * remake argument parsing; fix errors
    
    * fix error
    
    * fix pkgs not being given to function
    
    * remake text for multi-package support
    
    * fix using wrong var
    
    * fix stuff
    
    * fix different but same stuff
    
    * change add to remove in remove.py
    
    Co-authored-by: Fjorge <FjorgeTheHuman@github.com>

--- libtwirl/__main__.py
+++ libtwirl/__main__.py
@@ -1,24 +1,25 @@
 from libtwirl.errors import IncorrectArgumentFormatError
 from libtwirl.core.install import pkg_install
+from libtwirl.core.remove import pkg_remove
 from sys import argv
 
 args = argv[1:]
+pkgs = args[1:]
 
-flags = [
-    "install",
-    "remove",
-    "purge", 
-    "search"
-]
+# List of actions
+operations = {
+	"install": pkg_install,
+	"remove": pkg_remove,
+	"purge": None,
+	"search": None
+}
 
-count = 0
-try:
-    for arg in args: 
-        if args[count] not in flags and count != 1:
-            raise IncorrectArgumentFormatError(f"Unknown argument '{args[count]}'. (Valid arguments are {', '.join(flags)})")
-        count += 1
-    if count != 2:
-        raise IncorrectArgumentFormatError(f"Incorrect amount of operations provided. (Expected 2, got {count})")
-except Exception as E:
-    raise SystemExit()
-pkg_install(argv[2])
+# Check for argument errors
+if not operations[args[0]]:
+	raise IncorrectArgumentFormatError(f"Unknown operation '{args[0]}'. Valid operations are {', '.join(operations.keys())}.")
+if not len(args) > 1:
+	raise IncorrectArgumentFormatError("No packages specified.")
+
+# Do operations
+operations[args[0]](pkgs)
+
--- libtwirl/core/remove.py
+++ libtwirl/core/remove.py
@@ -4,43 +4,38 @@ from libtwirl.core.database import search_repos
 from libtwirl.core.database import fetch_local
 from libtwirl.core.database import fetch_handle
 
-def pkg_install(pkgs, options = None): # Function to install packages
+def pkg_remove(pkgs, options = None): # Function to uninstall packages
 	# Fetch important objects
 	localdb = fetch_local()
 	handle = fetch_handle()
 	targets = []
 	for name in pkgs:
-		# Check if the package is already installed before installing
-		if localdb.get_pkg(name):
-			print(f"WARNING: Package {name} is already installed.")
-		# Find package in the provided sync repos
-		package = search_repos(name)
-		# Make sure the package exists before installing
-		if package is None:
+		# Find the package in the localdb
+		package = localdb.get_pkg(name)
+		# Check if the package is installed
+		if not package:
 			raise PackageNotFoundError(name)
-		else: # Set the package to the first index of the array since we know it exists
-			package = package[0]
-		# Add the package to the list to install
+		# Add the package to the list of packages to install
 		targets.append(package)
 	# Print a list of packages to install
 	formatted_targets = []
 	[formatted_targets.append(f"{pkg.db.name}/{pkg.name}-{pkg.version}") for pkg in targets]
 	print('\n'.join(formatted_targets))
-	# Install the packages
+	# Remove the packages
 	t = handle.init_transaction()
-	[t.add_pkg(pkg) for pkg in targets]
+	[t.remove_pkg(pkg) for pkg in targets]
 	try:
 		# Start transaction
-		print("Starting installation.")
+		print("Starting removal.")
 		t.prepare()
 		t.commit()
 	except:
 		# Handle error if package fails to install and unlock dbs
 		t.release()
-		raise Exception("Package(s) failed to install.")
+		raise Exception("Package(s) failed to uninstall.")
 	# Unlock dbs after transaction
 	t.release()
-	print("Installed all targets.")
+	print(f"Removed all targets.")
 	# Update the local database
 	fetch_local()
 

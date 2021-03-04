a27f2023cad390004641a2b957311546c8cad783
Fjorge <30599715+FjorgeTheHuman@users.noreply.github.com>
2021-03-04 01:10:02 +0000

    add register_repos(), more comments (#2)

--- libtwirl/__init__.py
+++ libtwirl/__init__.py
@@ -1,12 +1,17 @@
 import pyalpm
 from pyalpm import Handle
+# TODO (POSSIBLY): wrap all in a single function for a frontend to use instead (and allow for changing root dir)
 handle = Handle("/", "/var/lib/pacman")
 
-# Local DB
+# Fetch the local database
+# Preferred if other functions do it instead themselves of relying on this variable for updatedness
 localdb = handle.get_localdb()
 
-# Synced DBs
-handle.register_syncdb("core", pyalpm.SIG_DATABASE_OPTIONAL)
+def register_repos(repolist, siglvl = pyalpm.SIG_DATABASE_OPTIONAL): # Function to register synced repositories
+	for reponame in repolist:
+		# Register repositories into the handle
+		# TODO (POSSIBLY): Add a way to specify the signature level of repositories (independently)
+		handle.register_syncdb(reponame, siglvl)
 
 def search_repos(pkgname, find_provides = True): # Function to search the synced repositories
 	syncdbs = handle.get_syncdbs()
@@ -20,7 +25,7 @@ def search_repos(pkgname, find_provides = True): # Function to search the synced
 	# If find_provides is false, skip this step
 	if find_provides:
 		# Iterate through list again to find provides
-		# This is to make sure the exact package, if it exists, always shows up first
+		# Iterating twice is to make sure the exact package, if it exists, always shows up first
 		for repo in syncdbs:
 			pkgs = repo.search(pkgname)
 			# Iterate though broader list to find package providers
@@ -32,5 +37,10 @@ def search_repos(pkgname, find_provides = True): # Function to search the synced
 		# Return the list of found packages
 		return packages
 	else:
-		return None
+		# Return None if there were no packages found
+		return None # Would it be preferred if this only returned an empty list instead of None?
   
+# Register synced repositories automatically for now
+# Preferred if a frontend does it themselves, see TODO below
+# TODO: parse config to find repositories to register (or make the frontend handle it) instead of arbitrary repo list at end
+register_repos(["core", "extra", "community"])
